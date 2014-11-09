d1e54bbddeab419224733a07968c4063d2b361dd
Massimo Musumeci <massimo@denali.london>
2014-11-09 19:40:36 +0100

    optimized for templates

--- my_application/app.py
+++ my_application/app.py
@@ -1,16 +1,16 @@
-from flask import Flask
-import jinja2
-app = Flask(__name__)
+import flask
+##import jinja2
+
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-        templateLoader = jinja2.FileSystemLoader( searchpath="/" )
-        templateEnv = jinja2.Environment( loader=templateLoader )
-        TEMPLATE_FILE = "/my_application/app.tpl"
-        template = templateEnv.get_template( TEMPLATE_FILE )
-        templateVars = { "title" : "The Test Example","description" : "A simple inquiry of function." }
-        outputText = template.render( templateVars )
-        return outputText
+	return flask.render_template('app.html', title="Example-title", description="Example-description")
+
+@app.route("/staticfile")
+def staticfile():
+	staticurl=flask.url_for('static', filename='examplepdf.pdf')
+	return staticurl
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
